27e3db5d5f21ea4cd66694d874a44d3a8452f73c
cbiale <claudio_biale@yahoo.com.ar>
2019-10-07 10:40:56 -0300

    Create ejemplo_4.js

---
+++ ejemplo_4.js
@@ -0,0 +1,6 @@
+cadena = "Objetos II";
+console.log("Largo de cadena es: " + cadena.length);
+nuevaCadena = cadena.substring(1,3);
+console.log("Valor de nuevaCadena es: " + nuevaCadena);
+console.log("cadena pasada a minúsculas: " + cadena.toLowerCase());
+console.log("cadena pasada a mayúsculas: " + cadena.toUpperCase());
